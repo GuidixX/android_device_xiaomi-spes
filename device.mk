141fb2cf1595bf3fb0a8533381ca5f24e7e950ff
John Galt <johngaltfirstrun@gmail.com>
2023-10-04 09:55:59 +0530

    spes: utilize task_profiles.json + cgroups.json
    We have full kernel support for uclamp and all additional functionality
    provided on current profiles.

--- device.mk
+++ device.mk
@@ -582,6 +582,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# Task Profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
